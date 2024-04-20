07770a444c97cdedfa90fca43cefebe30d64bf97
Hsiao yen Liang <hsiaoyenlaing@ViviShare.local>
2024-04-20 16:57:13 +0800

    finish L2

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,80 @@
 // Image switcher code
-
 let myImage = document.querySelector('img');
 
+// Define an array of image sources
+let imageArray = [
+  'images/firefox-icon.png', 
+  'images/firefox2.png',
+  'images/img01.jpg',
+  'images/img02.jpg',
+];
+// image index cuurently displayed
+let imageIndex = 0;
+// image switcher code
 myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
-  } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
-  }
+  imageIndex = (imageIndex + 1) % imageArray.length;
+  myImage.setAttribute('src', imageArray[imageIndex]);
 }
 
 // Personalized welcome message code
-
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
-// time varaible
-var date = new Date();
-var local_time = d.toLocaleTimeString();
-
+// setting the user's name function
 function setUserName() {
-  let myName = prompt('Please enter your name.');// 立即輸出文字
-  
-  // 若沒有偵測到
+  let myName = prompt('Please enter your name.');
   if(!myName) {
     setUserName();
-  } 
-  //有偵測到
-  else {
+  } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + local_time + ' ' + myName;    
+    displayWelcomeMessage(myName);
   }
 }
 
+
+// function to display the welcome message
+function displayWelcomeMessage(name) {
+  // Get the current date and time
+  let currentDate = new Date();
+  let currentHour = currentDate.getHours();
+  let currentMinute = currentDate.getMinutes();
+
+  // Determine the appropriate greeting based on the time of day
+  let greeting = '';
+  if (currentHour < 12) {
+    greeting = 'Good morning';
+  } else if (currentHour < 18) {
+    greeting = 'Good afternoon';
+  } else {
+    greeting = 'Good evening';
+  }
+  // Update the heading with the personalized greeting, current time, and date
+  myHeading.innerHTML = `${greeting}, ${name}! It's currently ${currentHour}:${currentMinute < 10 ? '0' + currentMinute : currentMinute}.`;
+}
+
+// function to increment visitor counter
+function incrementCounter() {
+  let visitorCounter = localStorage.getItem('visitorCount');
+  if (!visitorCounter) {
+    visitorCounter = 0;
+  }
+  else {
+    visitorCounter = parseInt(visitorCounter);
+  }
+  localStorage.setItem('visitorCount', visitorCounter + 1);
+}
+// function to display the visitor counter
+function displayCounter() {
+  let visitorCount = localStorage.getItem('visitorCount');
+  if (visitorCount){
+    alert(`You are visitor number ${visitorCount}!`);
+  }
+  else {
+    alert('Welcome! you are the first visitor to this site');
+  }
+}
+
+// Check if the name data exists
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
@@ -41,6 +82,9 @@ if(!localStorage.getItem('name')) {
   myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
+// Set and display the welcome message when the button is clicked
 myButton.onclick = function() {
   setUserName();
+  incrementCounter();
+  displayCounter();
 }
